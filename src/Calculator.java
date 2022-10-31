c48f9b5598e909177933b92611e8cf197a957637
wabakee55 <116024846+wabakee55@users.noreply.github.com>
2022-11-01 00:51:26 +0500

    Update Calculator.java
    Added puncuation, changed comments to active verbs and nouns. Comments to describe each method including parameters and return.

--- src/Calculator.java
+++ src/Calculator.java
@@ -26,15 +26,16 @@
  * Date:  	10/21/22 
  */
 
-// Packages
+// Packages.
 import java.util.Scanner;
 
-// Start of class
+// Start of class.
 public class Calculator {
 	/**
 	 * This method contains the entire program.
 	 *
-	 *@param number An array of any numbers.
+	 *@param number An array of any numbers, sum, subtraction, product, quotient, 
+	 *average, and sign of array values. 
 	 */
 	public static void main(String[] args) throws Exception {
 		// Variables
@@ -44,13 +45,13 @@ public class Calculator {
 		double[] nums = new double[3];
 		Scanner scnr = new Scanner(System.in);
 		//boolean getNumbers = false;
-		// Calculator
+		// Calculator.
 		do {
-			// Input
+			// Input.
 			menuChoice = userInput(scnr); 
 			/*
-			 * Checks if the users choice is a valid option or not and checks if the user
-			 * inputed numbers in the first run before choosing anything else
+			 * Check if the users choice is a valid option or not and checks if the user
+			 * input numbers in the first run before choosing anything else.
 			 */
 			if ((nums.length == 0) && !menuChoice.equals("N") && !menuChoice.equals("X")) {
 				System.out.println("\nError: Please select option N from the menu first!\n");
@@ -59,31 +60,31 @@ public class Calculator {
 			} else if (menuChoice.equals("N") || menuChoice.equals("A") || menuChoice.equals("S")
 					|| menuChoice.equals("M") || menuChoice.equals("D") || menuChoice.equals("V")
 					|| menuChoice.equals("L") || menuChoice.equals("I") || menuChoice.equals("E")) {
-				// Processes the users choice
+				// Process the users choice.
 				switch (menuChoice) {
 					case "N":
-						// Gets the users numbers and puts them in a array for further use
+						// Get the users numbers and puts them in a array for further use.
 						nums = getUserNum(scnr);
 						break;
 					case "A":
-						// Prints out all added numbers inside the array
+						// Print out all added numbers inside the array.
 						System.out.println("\nAnswer: " + signToPrint(nums, "+") + " = " + sumOfArray(nums));
 						break;
 					case "S":
-						// Prints out all the numbers subtracted inside the array
+						// Print out all the numbers subtracted inside the array.
 						System.out.println("\nAnswer: " + signToPrint(nums, "-") + " = " 
 							+ subtractFromArray(nums));
 						break;
 					case "M":
-						// Prints out all the numbers multiplied inside the array
+						// Print out all the numbers multiplied inside the array.
 						System.out.println("\nAnswer: " + signToPrint(nums, "*") + " = "
 							+ String.format("%.3f", multiplyArray(nums)));
 						break;
 					case "D":
 						divided = divideArray(nums);
 						/*
-						 * Prints out all the numbers divided inside the array if number was not divided
-						 * by zero
+						 * Print out all the numbers divided inside the array if number was not divided
+						 * by zero.
 						 */
 						
 						if (divided == 3.1415926535) {
@@ -94,36 +95,36 @@ public class Calculator {
 						}
 						break;
 					case "V":
-						// Prints out the average of all numbers inside the array
+						// Print out the average of all numbers inside the array.
 						System.out.println("\nAnswer: (" + signToPrint(nums, "+") + ") / " + nums.length + " = "
 								+ String.format("%.3f", averageOfArray(nums)));
 						break;
 					case "L":
-						// Prints out the largest number from the array
+						// Print out the largest number from the array.
 						System.out.println("\nThe largest number is " + largestFromArray(nums));
 						break;
 					case "I":
-						// Calls the method which will print if the numbers in the array are positive or
-						// negative
+						// Call the method which will print if the numbers in the array are positive or
+						// negative.
 						System.out.println("\n" + signOfArray(nums));
 						;
 						break;
 					case "E":
-						// Prints out the smallest number from the array
+						// Print out the smallest number from the array.
 						System.out.println("\nThe smallest number is " + smallestFromArray(nums));
 						break;
 					default:
-						// Prints out an error if one of the cases didn't match
+						// Print out an error if one of the cases didn't match.
 						System.out.println("Error: Invalid choice!");
 				}
 				/*
-				 * Prints an error if the user didn't select one of the give options from the
+				 * Print an error if the user didn't select one of the give options from the
 				 * menu.
 				 */
 			} else {
 				System.out.println("\nError: Please pick one of the list options and try again.\n");
 			}
-			// Ends the loop if the user selects "x"
+			// End the loop if the user selects "x".
 		} while (!menuChoice.equals("X"));
 		// Exit message.
 		System.out.println("\nThank you for using Team 2's calculator! Good-bye!");
@@ -134,27 +135,31 @@ public class Calculator {
 	/**
 	 *This method will find the average of the elements in the array.
 	 *
-	 *@param numbers An array of any numbers.
-	 *@return The average of the numbers.
+	 *@param numbers An array of any numbers, sum, average of array values.
+	 *@return The average of the array values.
 	 */
 	public static double averageOfArray(double[] nums) {
 		// Variables
 		double sum = 0;
 		double average = 0;
-		// Loops through nums and adds elements then divides by 3
+		// Loop through nums and adds elements then divides by array length.
 		for (int i = 0; i < nums.length; i++) {
 			sum = sum + nums[i];
 			average = sum / nums.length;
 		}
-		// Returns the value
+		// Return the value.
 		return average;
 	}
 
-	// This method will call divide all the numbers in the array
+	/* This method will call divide all the numbers in the array.
+	 *
+	 *@param numbers An array if any numbers, quotient of numbers in the array.
+	 *@return The quotient of the array values.
+	 */
 	public static double divideArray(double[] nums) {
-		// Declare variables
+		// Variables.
 		double totalDivide = 1;
-		// Loops though num array and divides each number
+		// Loop though num array and divides each number.
 		for (int i = 0; i < nums.length; i++) {
 			if (nums[0] == 0) {
 				return totalDivide = 0;
@@ -164,103 +169,129 @@ public class Calculator {
 				totalDivide /= nums[i];
 			}
 		}
-		// Returns the value
+		// Return the value.
 		return totalDivide;
 	}
 
-	// This method will ask the user to enter any amount of numbers they want
+	/* This method will ask the user to enter any amount of numbers they want.
+	 *
+	 *@param numbers An array of numbers.
+	 *@return An array of numbers.
+	 */
 	public static double[] getUserNum(Scanner scnr) {
-		// Asks the user to input x amount of values
+		// Ask the user to input x amount of values.
 		System.out.print("\nEnter 3 numbers separated with spaces: ");
 		/*
-		 * Takes in the users inputed value and takes out the spaces in between and
-		 * throwes it inside a string array
+		 * Take in the users inputed value and takes out the spaces in between and
+		 * throwes it inside a string array.
 		 */
 		String input = scnr.nextLine();
 		String[] strNumber = input.split(" ");
-		// Declared new double array and take in the length of previous string array
+		// Declare new double array and take in the length of previous string array.
 		double[] threeNumArray = new double[strNumber.length];
-		// Loops through the users input and converts it into double
+		// Loop through the users input and converts it into double.
 		for (int i = 0; i < threeNumArray.length; i++) {
 			threeNumArray[i] += Double.parseDouble(strNumber[i]);
 		}
-		// Returns the value
+		// Returns the value.
 		return threeNumArray;
 	}
 
 	/*
 	 * This method will take in the user inputted values and find the largest of
 	 * them and return that.
+	 *
+	 *@param number An array of numbers.
+	 *@return Largest value of numbers in the array.
 	 */
 	public static double largestFromArray(double[] nums) {
-		// Declared needed varables
+		// Variables.
 		double largestNum = 0;
 		/*
-		 * Loops through nums values and checks/updated largestNum if a number is
-		 * greater than the stored value
+		 * Loop through nums values and checks/updated largestNum if a number is
+		 * greater than the stored value.
 		 */
 		for (int i = 0; i < nums.length; i++)
 			if (largestNum < nums[i]) {
 				largestNum = nums[i];
 			}
-		// Returns the value
+		// Return the value.
 		return largestNum;
 	}
 
 	/*
-	 * This method will take in the user inputed values and find the largest of
-	 * them and return that.
+	 * This method will take in the user inputed values and calculate the quotient.
+	 * 
+	 *@param numbers An array of numbers, quotient of the numbers in the array.
+	 *@return Quotient of the numbers in the array.
 	 */
 	public static double multiplyArray(double[] nums) {
-		// Declared needed varables
+		// Variables.
 		double totalMulti = 1;
-		// Loops through nums values and multiplies them
+		// Loop through nums values and multiplies them.
 		for (int i = 0; i < nums.length; i++) {
 			totalMulti *= nums[i];
 		}
-		// Returns the value
+		// Return the value.
 		return totalMulti;
 	}
 
-	// This method will find the sign of each number in the array
+	/*
+	 *This method will find the sign of each number in the array.
+	 *
+	 *@param numbers An array of numbers.
+	 *@return The sign of the numbers in the array.
+	 */
 	public static double signOfArray(double[] nums) {
-		// Declare variables
+		// Variables.
 		double sign = 0;
-		// Determines if each number in array is either positive or negative and prints
-		// accordingly
+		/* 
+		 * Determines if each number in array is either positive or negative and prints
+		 * accordingly.
+		 */ 
 		for (int i = 0; i < nums.length; i++)
 			if (nums[i] >= 0) {
 				System.out.println("The number in position " + nums[i] + " is positive. ");
 			} else {
 				System.out.println("The number in position " + nums[i] + " is negative.");
 			}
-		// Returns value
+		// Return the sign.
 		return sign;
 	}
 
-	// This method will figure out what math sign needs to be added between numbers
+	/*
+	 *This method will figure out what math sign needs to be added between numbers.
+	 *
+	 *@param numbers An array of numbers.
+	 *@return Operation sign.
+	 */
 	public static String signToPrint(double[] nums, String sign) {
-		// Declared Variable
+		// DVariable.
 		String signForPrint = "";
-		// Loops through all the numbers inside the array and add the corresponding
-		// signs
+		/*
+		 * Loops through all the numbers inside the array and add the corresponding
+		 * signs.
+		 */
 		for (int i = 0; i < nums.length; i++) {
 			signForPrint = signForPrint + nums[i] + " " + sign + " ";
 		}
-		// Removes the last extra sign added in the for-loop and prints the final answer
+		// Removes the last extra sign added in the for-loop and prints the final answer.
 		signForPrint = signForPrint.substring(0, signForPrint.length() - 3);
 
 		return signForPrint;
 	}
 
 	/*
-	 * This method will take in the values user inputed and will multiply and return
-	 * the result.
+	 * This method will take in the values user inputed and will find the smallest number in 
+	 * the array and return the result.
+	 *
+	 *@param numbers An array of numbers.
+	 *@return Smallest value in array.
 	 */
 	public static double smallestFromArray(double[] nums) {
-		// Declare a variable
+		// Variable.
 		double smallestNum;
-		// Loops thorugh nums and finds the smallest value in the array
+		// Loop thorugh nums and finds the smallest value in the array.
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = i; j < nums.length; j++) {
 				if (nums[i] > nums[j]) {
@@ -270,42 +301,50 @@ public class Calculator {
 				}
 			}
 		}
-		// Returns value
+		// Return value.
 		return nums[0];
 	}
 
 	/*
 	 * This method will take in the user inputted values and subtract to fund the
 	 * final value and return it.
+	 *
+	 *@param numbers An array of numbers, difference of numbers in the array.
+	 *@return The difference of numbers in the array.
 	 */
 	public static double subtractFromArray(double[] nums) {
-		// Declared needed varables
+		// Variables.
 		double totalSubtract = 0;
-		// Loops through nums values and subtracts them
+		// Loop through nums values and subtracts them.
 		for (int i = 0; i < nums.length; i++) {
 			totalSubtract -= nums[i];
 		}
-		// Returns the value
+		// Return the differnce.
 		return totalSubtract;
 	}
 
-	// This method finds the sum of elements in an array
+	/*
+	 *This method finds the sum of the numbers in the array.
+	 *
+	 *@param numbers An array of numbers, sum of numbers in the array.
+	 *@return The sum of numbers in the array.
+	 */
 	public static double sumOfArray(double[] nums) {
-		// Declared variable
+		// Variable.
 		double sum = 0;
-		// Loops through nums and adds each element in array
+		// Loop through nums and adds each element in array.
 		for (int i = 0; i < nums.length; i++) {
 			sum = sum + nums[i];
 		}
-		// Returns the value
+		// Return the value.
 		return sum;
 	}
 
-	// Prints the calculator menu and gets the users choice
+	// Print the calculator menu and gets the users choice.
 	public static String userInput(Scanner scnr) {
-		// Declaring String Variable;
+		// String Variable.
 		String menuChoice;
-		// Menu options for user to choose.
+		// Print menu options for user to choose.
 		System.out.println("\nCalculator Menu\n");
 		System.out.println("Get 3 (N)umbers.");
 		System.out.println("(A)dd the numbers.");
@@ -318,11 +357,11 @@ public class Calculator {
 		System.out.println("Determine the s(I)gn of each number.");
 		System.out.println("E(X)it.\n");
 		System.out.print("Choice: ");
-		// Take in the users choice and uppercases the value incase it was lowercase
+		// Take in the users choice and uppercases the value incase it was lowercase.
 		menuChoice = scnr.nextLine();
 		menuChoice = menuChoice.toUpperCase();
-		// Returns the choice
+		// Return the user choice.
 		return menuChoice;
 	}
 
-}// Class ends here
+}// Class ends here.
